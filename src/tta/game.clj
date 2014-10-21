294ee8d4be8a318147688dd44d37291d5fad6af3
Laura Leppänen <laura.leppanen@iki.fi>
2014-10-21 19:39:15 +0300

    Added a function for handling the production phase.
    Produces food from farms.

--- src/tta/game.clj
+++ src/tta/game.clj
@@ -4,7 +4,8 @@
 (def initial-player-state
   {:buildings {:temple 1
                :farm 2
-               :mine 2}})
+               :mine 2}
+   :consumables {:food 0}})
 
 (defn create-player [a-name]
   (assoc initial-player-state
@@ -40,6 +41,26 @@
   (if (last-players-turn? game)
     (next-round game)
     (next-player game)))
+
+(defn current-player [game]
+  (get (:players game)
+       (:current-player game)))
+
+(defn production-phase [game]
+  "Updates the current player's board state
+   according to the rules of the production phase."
+  (let [player (current-player game)
+        updated-player
+          (update-in player [:consumables :food]
+                     (fn [food]
+                       (+ food (get-in player [:buildings :farm]))))]
+    (assoc-in game [:players (:current-player game)] updated-player)))
+
+(fact
+  (get-in (production-phase sample-game-state)
+          [:players 0 :consumables :food]) => 2
+  (get-in (production-phase (assoc sample-game-state :current-player 1))
+          [:players 1 :consumables :food]) => 2)
 
 (let [game sample-game-state
       game2 (assoc game :current-player 2)]
