10c71d6e597f13f10913a2ba10677050e15ba381
Ilmari Vacklin <ilmari.vacklin@reaktor.fi>
2015-01-15 20:36:20 +0200

    Update deps, faster build

--- project.clj
+++ project.clj
@@ -10,20 +10,21 @@
                  [environ "0.5.0"]
                  [jayq "2.5.2"]
                  [hiccups "0.3.0"]
-                 [org.clojure/clojurescript "0.0-2371"]
+                 [org.clojure/clojurescript "0.0-2665"]
                  [org.clojure/algo.monads "0.1.5"]]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.2.1"]
-            [lein-cljsbuild "1.0.3"]
+            [lein-cljsbuild "1.0.4"]
             [lein-ring "0.8.13"]]
   :hooks [leiningen.cljsbuild]
   :uberjar-name "tta.jar"
   :profiles {:production {:env {:production true}}
              :dev {:dependencies [[midje "1.6.3"]]
                    :plugins [[lein-midje "3.1.3"]]}}
-  :cljsbuild {:builds [{:source-paths ["cljs"]
+  :cljsbuild {:builds [{:source-paths ["cljs", "target/classes"]
                         :jar true
                         :compiler {:output-to "resources/public/js/app.js"
                                    :optimizations :whitespace
+                                   :cache-analysis true
                                    :pretty-print true}}]}
   :ring {:handler tta.web/app})
