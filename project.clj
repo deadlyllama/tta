866c3f2cfaa3542617c270240927c3aadc61a53a
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2015-08-05 19:42:27 +0300

    Update deps

--- project.clj
+++ project.clj
@@ -4,24 +4,23 @@
   :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [compojure "1.1.8"]
-                 [ring/ring-jetty-adapter "1.2.2"]
-                 [ring-middleware-format "0.4.0"]
-                 [environ "0.5.0"]
-                 [jayq "2.5.2"]
+                 [compojure "1.4.0"]
+                 [ring-middleware-format "0.5.0"]
+                 [environ "1.0.0"]
+                 [jayq "2.5.4"]
                  [hiccups "0.3.0"]
-                 [org.clojure/clojurescript "0.0-2665"]
+                 [org.clojure/clojurescript "1.7.28"]
                  [org.clojure/algo.monads "0.1.5"]]
   :main tta.web
   :aot [tta.web]
   :min-lein-version "2.0.0"
-  :plugins [[environ/environ.lein "0.2.1"]
-            [lein-cljsbuild "1.0.4"]
-            [lein-ring "0.8.13"]]
+  :plugins [[environ/environ.lein "0.3.1"]
+            [lein-cljsbuild "1.0.6"]
+            [lein-ring "0.9.6"]]
   :hooks [leiningen.cljsbuild]
   :uberjar-name "tta.jar"
   :profiles {:production {:env {:production true}}
-             :dev {:dependencies [[midje "1.6.3"]]
+             :dev {:dependencies [[midje "1.7.0"]]
                    :plugins [[lein-midje "3.1.3"]]}}
   :cljsbuild {:builds [{:source-paths ["cljs"]
                         :jar true
